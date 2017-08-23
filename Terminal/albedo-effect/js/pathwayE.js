c870c38b3e889767b14304e5ebba3c51f518288c
mhrice <matthewrice11@yahoo.com>
2017-08-23 13:40:19 -0700

    Fixed touch input

--- Terminal/albedo-effect/js/pathwayE.js
+++ Terminal/albedo-effect/js/pathwayE.js
@@ -1,24 +1,35 @@
-
-/* Swipe Functionality */
-// 
+//
+// /* Swipe Functionality */
+//
 // var myElement = document.getElementById('swipe');
 // var mc = new Hammer(myElement);
 // mc.get('swipe').set({ direction: Hammer.DIRECTION_ALL });
 // // listen to events...
 // mc.on("swipeleft swiperight swipeup swipedown", function(ev) {
+//   console.log("YO");
 //     switch (ev.type) {
 //       case "swipeleft":
-//         window.location.href = "gallery/index.html";
+//       jump("firstPage")
+//       //document.getElementById(h).scrollIntoView();
+//         //window.location.href = "gallery/index.html";
 //         break;
 //       case "swiperight":
-//         window.location.href = "leftPage.html";
+//         //window.location.href = "leftPage.html";
+//         jump("firstPage")
 //         break;
 //       case "swipeup":
-//         window.location.href = "bottomPage.html";
+//         //window.location.href = "bottomPage.html";
+//         jump("firstPage")
 //         break;
 //       case "swipedown":
-//         window.location.href = "topPage.html";
+//         jump("firstPage")
+//         //window.location.href = "topPage.html";
 //       default:
 //
 //     }
 // });
+// function jump(h){
+//     var url = location.href;               //Save down the URL without hash.
+//     location.href = "#"+h;                 //Go to the target element.
+//     history.replaceState(null,null,url);   //Don't like hashes. Changing it back.
+// }​
