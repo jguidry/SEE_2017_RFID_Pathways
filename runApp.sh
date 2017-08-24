4a9d8c78fb359776e4b37023fc3382ea1daaa5bf
mhrice <matthewrice11@yahoo.com>
2017-08-24 21:07:19 +0000

    Updated script again

--- runApp.sh
+++ runApp.sh
@@ -10,7 +10,7 @@ wget -q --tries=10 --timeout=20 --spider http://google.com
 if [[ $? -eq 0 ]];
 #then chromium-browser --kiosk --incognito https://birchaquarium-fd036.firebaseapp.com
 then 
-chromium-browser --incognito file:///home/pi/SEE_2017_RFID_Pathways/Terminal/terminal-home.html &
+#chromium-browser --kiosk --incognito file:///home/pi/SEE_2017_RFID_Pathways/Terminal/terminal-home.html &
 chromium-browser --incognito file:///home/pi/SEE_2017_RFID_Pathways/Terminal/terminal-home.html &
 break
 fi
